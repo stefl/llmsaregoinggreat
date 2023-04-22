6e8eefe5f76b6223618337f01c3d3e16fda1b33d
Stef Lewandowski <stef@stef.io>
2023-04-22 13:34:34 +0100

    Hard code globals

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,13 +1,7 @@
 export const getGlobalData = () => {
-  const name = process.env.BLOG_NAME
-    ? decodeURI(process.env.BLOG_NAME)
-    : 'LLMS are Going Great!';
-  const blogTitle = process.env.BLOG_TITLE
-    ? decodeURI(process.env.BLOG_TITLE)
-    : 'LLMS are Going Great!';
-  const footerText = process.env.BLOG_FOOTER_TEXT
-    ? decodeURI(process.env.BLOG_FOOTER_TEXT)
-    : '';
+  const name = 'LLMS are Going Great!';
+  const blogTitle = 'LLMS are Going Great!';
+  const footerText = '';
 
   return {
     name,
